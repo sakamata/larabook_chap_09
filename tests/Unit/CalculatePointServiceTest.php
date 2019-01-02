a18039b1e5f7b8baba9cccc8b87e8074be3a3a76
Kazushi Takamura <sakamatapd5@gmail.com>
2019-01-03 03:37:46 +0900

    Add: 9-1-4 4　データプロバイダの活用　完了時点

--- tests/Unit/CalculatePointServiceTest.php
+++ tests/Unit/CalculatePointServiceTest.php
@@ -38,4 +38,27 @@ class CalculatePointServiceTest extends TestCase
         $this->assertSame(10, $result); // $result が10である事を検証
     }
 
+    // 9.1.4.1 データプロバイダメソッドの例
+    public function dataProvider_for_calcPoint() : array
+    {
+        return [
+            '購入金額が0なら0ポイント' => [0, 0],
+            '購入金額が999なら0ポイント' => [0, 999],
+            '購入金額が1000なら10ポイント' => [10, 1000],
+            '購入金額が9999なら99ポイント' => [99, 9999],
+            '購入金額が10000なら200ポイント' => [200, 10000],
+        ];
+    }
+
+    // 9.1.4.2
+    /**
+     * @test
+     * @dataProvider dataProvider_for_calcPoint
+     */
+    public function calcPoint(int $expected, int $amount)
+    {
+        $result = CalculatePointService::calcPoint($amount);
+        $this->assertSame($expected, $result); 
+    }
+
 }
