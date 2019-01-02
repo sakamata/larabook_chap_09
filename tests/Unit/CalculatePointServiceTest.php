76b56e6aafc3c10fe3c52259327e18d447a5a22c
Kazushi Takamura <sakamatapd5@gmail.com>
2019-01-03 03:05:25 +0900

    Add: 9-1-3 テストメソッドの実装　完了時点

--- tests/Unit/CalculatePointServiceTest.php
+++ tests/Unit/CalculatePointServiceTest.php
@@ -5,6 +5,7 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Services\CalculatePointService;
 
 class CalculatePointServiceTest extends TestCase
 {
@@ -18,4 +19,23 @@ class CalculatePointServiceTest extends TestCase
     {
         $this->assertTrue(true);
     }
+
+    /**
+     * @test
+     */
+    public function calcPoint_購入金額が0ならポイントは0()
+    {
+        $result = CalculatePointService::calcPoint(0);
+        $this->assertSame(0, $result); // $result が0である事を検証
+    }
+
+    /**
+     * @test
+     */
+    public function calcPoint_購入金額が1000ならポイントは10()
+    {
+        $result = CalculatePointService::calcPoint(1000);
+        $this->assertSame(10, $result); // $result が10である事を検証
+    }
+
 }
