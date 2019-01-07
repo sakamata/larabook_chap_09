fa3ebae22352edeb2f618525852b46827980af96
Kazushi Takamura <sakamatapd5@gmail.com>
2019-01-07 17:52:04 +0900

    Add: 9-2-5　モックによるテスト　完了時点

--- tests/Unit/AddPoint/EloquentCustomerPointEventTest.php
+++ tests/Unit/AddPoint/EloquentCustomerPointEventTest.php
@@ -21,21 +21,25 @@ class EloquentCustomerPointEventTest extends TestCase
     {
         $customerId = 1;
         // (2) テストデータ登録
+        // これまで出てないが customers テーブルのモデルで1レコード作成
+        // name入れてないのはfactory側でFakerで自動生成される為っぽい
         factory(EloquentCustomer::class)->create([
             'id' => $customerId,
         ]);
-
+        // テスト対象メソッドの実行
+        // コンストラクタの引数（DBカラム名）をそのままの順番で入れてる
         $event = new PointEvent(
             $customerId,
             '加算イベント',
             100,
             Carbon::create(2018, 8, 4, 12, 34, 56)
         );
-        // (3) テスト対象メソッドを実行
+        // DBに登録
         $eloquet = new EloquentCustomerPointEvent();
         $eloquet->register($event);
 
         // (4) データベースレコードのアサーション
+        // 今登録したレコードがあるかを確認
         $this->assertDatabaseHas('customer_point_events', [
             'customer_id' => $customerId,
             'event'       => $event->getEvent(),
